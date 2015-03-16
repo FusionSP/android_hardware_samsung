0d86d9ee16bb4b66fa3c02a003dc8d305eea2e69
BlackDragon <blackdragon.fusionteam@gmail.com>
2015-03-16 01:23:03 +0100

    Squash multiple commits by CM.
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>

--- exynos4/hal/libcamera/Android.mk
+++ exynos4/hal/libcamera/Android.mk
@@ -1,5 +1,3 @@
-ifneq ($(TARGET_PROVIDES_CAMERA_HAL),true)
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -7,8 +5,7 @@ include $(CLEAR_VARS)
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.product.board>.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include \
-	system/media/camera/include
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
 
 LOCAL_SRC_FILES:= \
 	SecCamera.cpp SecCameraHWInterface.cpp
@@ -39,5 +36,3 @@ LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
